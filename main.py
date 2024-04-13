193a515ee3a1510935056a3c948e330dec14150f
Sofia Michailidu <sofia@michailidu.cz>
2024-04-13 17:55:57 +0200

    measure execution time

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
+from datetime import datetime
+
 from ultralytics import YOLO
 
 
-def run():
+def run_model(model, config_name, project):
     test_path = r"data\images\test"
     epochs = 300
     batch_size = 128
-    patience = 16
+    patience = 12
     seed = 16
     name_train = "exp1train"
     name_test = "exp1test"
 
-    # 8k dataset
-    model = YOLO("yolov9c.pt")
+    start = datetime.now()
     model.train(data="config_8k.yaml", epochs=epochs, batch=batch_size, project="8k", name=name_train,
                 deterministic=True, seed=seed, profile=True, plots=True, patience=patience)
-    model.export(format="onnx")
-    model.predict(source=test_path, save=True, save_txt=True, save_conf=True, project="8k", name=name_test)
+    end = datetime.now()
 
-    # 14k dataset
-    model.train(data="config_14k.yaml", epochs=epochs, batch=batch_size, project="14k", name=name_train,
-                deterministic=True, seed=seed, profile=True, plots=True, patience=patience)
-    model.export(format="onnx")
-    model.predict(source=test_path, save=True, save_txt=True, save_conf=True, project="14k", name=name_test)
+    with open("time.txt", "a") as file:
+        file.write(f"Project: {project}, Config: {config_name}, Epochs: {epochs}, Time: {end - start}\n")
 
-    # full dataset
-    model.train(data="config_full.yaml", epochs=epochs, batch=batch_size, project="full", name=name_train,
-                deterministic=True, seed=seed, profile=True, plots=True, patience=patience)
     model.export(format="onnx")
-    model.predict(source=test_path, save=True, save_txt=True, save_conf=True, project="full", name=name_test)
+    model.predict(source=test_path, save=True, save_txt=True, save_conf=True, project=project, name=name_test)
+    return model
+
+
+def run():
+    model = YOLO("yolov9c.pt")
+    run_model(model, "config_8k.yaml", "8k")
+
+    run_model(model, "config_14k.yaml", "14k")
+
+    run_model(model, "config_full.yaml", "full")
 
 
 if __name__ == "__main__":
